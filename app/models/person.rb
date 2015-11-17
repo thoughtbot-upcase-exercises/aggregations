5ed816fa7b2d94de970253a7fa34aee63e4c1d8b
Joe Ferris <jferris@thoughtbot.com>
2015-11-17 10:10:28 -0500

    Add exercises for aggregations

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,12 @@ class Person < ActiveRecord::Base
   belongs_to :role
   belongs_to :manager, class_name: "Person", foreign_key: :manager_id
   has_many :employees, class_name: "Person", foreign_key: :manager_id
+
+  def self.maximum_salary_by_location
+    {}
+  end
+
+  def self.managers_by_salary_difference
+    all
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,52 @@
 require "spec_helper"
 
 describe Person do
+  describe ".maximum_salary_by_location" do
+    it "finds the highest salary at each location" do
+      pending "Implement maximum_salary_by_location to make this spec pass"
+
+      [50_000, 60_000].each do |highest_salary|
+        location = create(:location, name: "highest-#{highest_salary}")
+        create(:person, location: location, salary: highest_salary - 1)
+        create(:person, location: location, salary: highest_salary)
+      end
+
+      result = Person.maximum_salary_by_location
+
+      expect(find_names(result)).to eq(
+        "highest-50000" => 50_000,
+        "highest-60000" => 60_000
+      )
+    end
+  end
+
+  def find_names(hash_by_id)
+    hash_by_id.inject({}) do |hash_by_name, (id, value)|
+      name = Location.find(id).name
+      hash_by_name.merge(name => value)
+    end
+  end
+
+  describe ".managers_by_salary_difference" do
+    it "orders managers by salary difference over their employees" do
+      pending "Implement managers_by_salary_difference to make this spec pass"
+
+      [
+        [50_000, 10_000],
+        [50_000, -5_000],
+        [45_000, 20_000]
+      ].each do |(salary, spread)|
+        manager = create(:person, salary: salary, name: "spread-#{spread}")
+        create(:person, manager: manager, salary: salary - spread * 1)
+        create(:person, manager: manager, salary: salary - spread * 2)
+        create(:person, manager: manager, salary: salary - spread * 3)
+      end
+
+      result = Person.managers_by_salary_difference
+      puts result.to_sql
+
+      expect(result.map(&:name)).
+        to eq(%w(spread-20000 spread-10000 spread--5000))
+    end
+  end
 end
